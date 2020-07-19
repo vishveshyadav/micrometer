9365c6743d0f8936e3962bea8c8a50f37876030a
Vishvesh Yadav <vishveshyadav@MacBook-Pro.local>
2020-07-19 18:58:54 +0530

    Adding support for Junit5

---
+++ src/test/java/com/example/experiment/metric/runner/TestClass.java
@@ -0,0 +1,46 @@
+package com.example.experiment.metric.runner;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.*;
+
+@ExtendWith(SpringExtension.class)
+public class TestClass {
+
+    @Test
+    public void test() {
+
+        ThreadFactory threadFactory = new BasicThreadFactory.Builder().namingPattern("CustomThread -%d").build();
+        ExecutorService threadpool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(), threadFactory);
+
+        Runnable runnable = () -> {
+            System.out.println("Hello running inside :" + Thread.currentThread().getName());
+        };
+
+        Callable<Integer> callable = () -> {
+            System.out.println("Hello running inside :" + Thread.currentThread().getName());
+            return 100;
+        };
+
+        Mono.fromFuture(CompletableFuture.runAsync(runnable, threadpool)
+                .thenAccept(value -> System.out.println("Processed Inside runnable " + Thread.currentThread().getName())))
+                .doOnError(error -> System.out.println("Error caught: " + error.getMessage()))
+                .subscribe();
+
+        Mono.fromCallable(callable)
+                .subscribeOn(Schedulers.fromExecutorService(threadpool))
+                .map(integer -> {
+                    System.out.println("Running on "+Thread.currentThread().getName());
+                    return integer;
+
+                })
+                .doOnNext(integer -> System.out.println("Do On next on "+ Thread.currentThread().getName()))
+                .subscribe(integer -> System.out.println(integer + " Result Obtained on "+ Thread.currentThread().getName()));
+
+    }
+}
